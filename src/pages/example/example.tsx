26ee7ded0eb41d7f137d3e905526874d1e526f2e
Sotatek-vinhphan <vinh.phan@sotatek.com>
2022-09-16 18:00:45 +0700

    first commit

---
+++ src/pages/example/example.tsx
@@ -0,0 +1,12 @@
+import {Divider} from "antd";
+
+export function Example() {
+    return <>
+        <div>
+            <h1>Example</h1>
+            <Divider orientation="left" plain>
+                Left Text
+            </Divider>
+        </div>
+    </>
+}
