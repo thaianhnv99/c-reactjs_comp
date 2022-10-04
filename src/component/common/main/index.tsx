d298a863d54d914ecbc6df70447c773227de6aea
thaianhnv99 <thaianhnguyen283@gmail.com>
2022-10-04 11:00:28 +0700

    init

--- src/component/common/main/index.tsx
+++ src/component/common/main/index.tsx
@@ -2,6 +2,7 @@ import {Container} from "@mui/material";
 import {AppBox} from "../../Base";
 
 export function Main({children}: any) {
+    const a = 'text'
     return (
         <AppBox color="gray10">
             <Container sx={{padding: '0 1rem'}}>
