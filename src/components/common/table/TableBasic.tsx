dc539f34c59662bbc1ee84b014766f2b673f6200
thaianhnv99 <thaianhnguyen283@gmail.com>
2023-06-09 18:29:28 +0700

    init

--- src/components/common/table/TableBasic.tsx
+++ src/components/common/table/TableBasic.tsx
@@ -2,29 +2,63 @@ import { TableBody, TableCell, TableHead, TableRow } from "@mui/material";
 import Paper from "@mui/material/Paper";
 import Table from "@mui/material/Table";
 import TableContainer from "@mui/material/TableContainer";
+import { useEffect, useState } from "react";
 
-const TableBasic = () => {
-    return ( <>
-    <TableContainer component={Paper} square>
-        <Table stickyHeader sx={{ minWidth: 650 }}>
-            <TableHead>
-                <TableRow>
-                    <TableCell colSpan={2}>A</TableCell>
-                    <TableCell>B</TableCell>
-                    <TableCell>C</TableCell>
-                </TableRow>
-            </TableHead>
-            <TableBody>
-            <TableCell component="th" scope="row">
-                Name
-              </TableCell>
-                <TableCell>ValueA</TableCell>
-                <TableCell>ValueB</TableCell>
-                <TableCell>ValueC</TableCell>
-            </TableBody>
-        </Table>
-    </TableContainer>
-    </> );
+interface ColumnProps<Item> {
+    field: keyof Item,
+    headerName: string,
+    groupBy?: string,
+    [key: string]: any
 }
- 
+
+interface TableBasicProps<Item> {
+    columns: ColumnProps<Item>[],
+    rows: Item[]
+}
+const TableBasic = <Item,>({ columns, rows }: TableBasicProps<Item>) => {
+    const [columnGroup, setColumnGroup] = useState([]);
+    useEffect(() => {
+        if (columns) {
+            //Map columns
+            const group = columns.reduce((result, currentValue) => {
+                if (currentValue?.groupBy) {
+                    return (result[currentValue?.groupBy] = result[currentValue?.groupBy] || []).push(currentValue)
+                }
+
+                return result;
+            }, {});
+
+            console.log('1111111111111', group);
+
+        }
+    }, [columns])
+    return (<>
+        <TableContainer component={Paper} square>
+            <Table stickyHeader sx={{ minWidth: 650 }}>
+                <TableHead>
+                    <TableRow>
+                        {
+                            columns.map((item, index) => {
+                                return <TableCell key={index}>{item.headerName}</TableCell>
+                            })
+                        }
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {
+                        rows.map((item, index) => {
+                            return <TableRow>
+                                {columns.map(col => {
+                                    return <TableCell key={index}>
+                                        {item[col.field] as string}
+                                    </TableCell>
+                                })}
+                            </TableRow>
+                        })}
+                </TableBody>
+            </Table>
+        </TableContainer>
+    </>);
+}
+
 export default TableBasic;
--- src/pages/comps/data-grid/DataGrid.tsx
+++ src/pages/comps/data-grid/DataGrid.tsx
@@ -1,9 +1,70 @@
 import TableBasic from "src/components/common/table/TableBasic";
 
-const DataGrid = () => {
-    return ( <>
-    <TableBasic/>
-        </> );
+interface TColumn {
+    field: keyof TData,
+    headerName: string,
+    groupBy?: string
 }
- 
+
+interface TData {
+    a1: string,
+    a2: string,
+    b: string,
+    c: string
+}
+const DataGrid = () => {
+    const row = [
+        {
+            a1: 'a1Name', a2: 'a2Name', b: 'bName', c: 'cName'
+        },
+        {
+            a1: 'a1Name', a2: 'a2Name', b: 'bName', c: 'cName'
+        },
+        {
+            a1: 'a1Name', a2: 'a2Name', b: 'bName', c: 'cName'
+        },
+        {
+            a1: 'a1Name', a2: 'a2Name', b: 'bName', c: 'cName'
+        },
+        {
+            a1: 'a1Name', a2: 'a2Name', b: 'bName', c: 'cName'
+        },
+    ];
+    const columns: TColumn[] = [
+        {
+            field: 'a1',
+            headerName: 'a1',
+            groupBy: 'a'
+        },
+        {
+            field: 'a2',
+            headerName: 'a2',
+            groupBy: 'a'
+        },
+        {
+            field: 'b',
+            headerName: 'b',
+        },
+        {
+            field: 'c',
+            headerName: 'c',
+        },
+        // {
+        //     field: 'name',
+        //     headerName: t('POLICY.POLICY_NAME'),
+        //     type: 'string',
+        //     flex: 1,
+        //     renderCell: (params) =>
+        //         RenderLink({
+        //             value: params.formattedValue,
+        //             url: `/policy/management/${params.id}/`,
+        //             target: '_self',
+        //         }),
+        // },
+    ]
+    return (<>
+        <TableBasic columns={columns} rows={row} />
+    </>);
+}
+
 export default DataGrid;
