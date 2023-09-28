05f61c17affd04ee971d0f4cb630e6ff00778c62
thaianhnv99 <thaianhnguyen283@gmail.com>
2023-09-28 19:05:29 +0700

    add

--- src/pages/react-query/List.tsx
+++ src/pages/react-query/List.tsx
@@ -10,7 +10,6 @@ import ReloadIcon from "src/icons/ReloadIcon";
 const List = () => {
   const [textSearch, setTextSearch] = useState<string>();
   const value = useDebounce(textSearch, 300);
-  const a = '123456'
 
   const { data, isError, isLoading, isFetching, isSuccess, refetch } = useQuery(
     [POST_CACHE_KEY.GET_LIST],
