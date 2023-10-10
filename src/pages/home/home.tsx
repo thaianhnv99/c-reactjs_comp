83a3f764489620d7ce86281efa7f589a11c911bc
thaianhnv99 <thaianhnguyen283@gmail.com>
2023-10-10 15:01:30 +0700

    iniy

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -45,7 +45,7 @@ export function Home() {
 
   return (
     <>
-      Home update v8
+      Home update v8.1
       <Button variant="contained" onClick={logout}>
         Logout{loading ? "..." : ""}
       </Button>
