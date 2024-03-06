5d32abd988585a24514e70ae0c9ebad44861a3db
thaianhnv99 <thaianhnguyen283@gmail.com>
2024-03-06 15:27:47 +0700

    fix lint

--- src/pages/example/example.test.tsx
+++ src/pages/example/example.test.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import { Example } from './Example';
+import { Example } from './example';
 
 describe('Home module', () => {
   test('Home renders correctly', async () => {
