2e5c03deeaac47e235d62c59d06df59bed56c8a4
thaianhnv99 <thaianhnguyen283@gmail.com>
2024-05-21 11:50:31 +0700

    fix

--- src/index.tsx
+++ src/index.tsx
@@ -68,3 +68,5 @@ enableMocking().then(() => {
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+//Test
